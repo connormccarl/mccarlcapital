60d5b62c744890c37d257671172c80ccba7ee39a
Connor McCarl <connormccarl@gmail.com>
2024-10-05 07:07:52 -0700

    blog coming soon

--- mccarlcapital-app/app/(main)/blog/page.tsx
+++ mccarlcapital-app/app/(main)/blog/page.tsx
@@ -32,7 +32,7 @@ const Blog = () => {
           </p>
         </div>
         <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          {posts.map((post) => (
+          {/*posts.map((post) => (
             <article key={post.id} className="flex max-w-xl flex-col items-start justify-between">
               <div className="flex items-center gap-x-4 text-xs">
                 <time dateTime={post.datetime} className="text-gray-500">
@@ -67,7 +67,8 @@ const Blog = () => {
                 </div>
               </div>
             </article>
-          ))}
+          ))*/}
+          <h2 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">Coming soon</h2>
         </div>
       </div>
     </div>
