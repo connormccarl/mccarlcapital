9e55656a280c821d41c87f1c728355f5c4532a56
Connor McCarl <connormccarl@gmail.com>
2024-10-09 07:20:32 -0700

    custom button update

--- mccarlcapital-app/components/CustomButton.tsx
+++ mccarlcapital-app/components/CustomButton.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Button } from '@headlessui/react'
 
-const CustomButton = ({ className, variant , children }: { variant?: string, children: React.ReactNode }) => {
+const CustomButton = ({ className, variant , children }: { className?: string, variant?: string, children: React.ReactNode }) => {
   return (
     <Button className={`${className} rounded bg-gold hover:bg-green hover:text-white px-4 py-2 text-sm font-semibold leading-6 text-white`}>
         {children}
